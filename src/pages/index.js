c1ce19e8ccd1688698bd62ec7f3d9fd69c680318
Francisco Martin <francisco.martin@ambevtech.com.br>
2021-02-09 20:32:09 -0300

    add material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone youtube</div>;
+  return (
+    <div>
+      Clone youtube
+      <Button variant="outlined" color="secondary">
+        Hello
+      </Button>
+    </div>
+  );
 }
